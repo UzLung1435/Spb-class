941e8b067ff785f483d9d6ac72588ba8d5709d66
Uz <2945921033@qq.com>
2024-06-14 15:38:23 +0800

    [Init] Init Commit

---
+++ src/main/kotlin/kt/uz/javasiliaosystem/JavaSiliaoSystemApplication.kt
@@ -0,0 +1,9 @@
+package kt.uz.javasiliaosystem
+
+import org.springframework.boot.runApplication
+
+class JavaSiliaoSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaSiliaoSystemApplication>(*args)
+}
---
+++ src/test/kotlin/kt/uz/javasiliaosystem/JavaSiliaoSystemApplicationTests.kt
@@ -0,0 +1,11 @@
+package kt.uz.javasiliaosystem
+
+import org.junit.jupiter.api.Test
+
+class JavaSiliaoSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
