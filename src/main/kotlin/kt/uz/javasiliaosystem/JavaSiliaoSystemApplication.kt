e81e110fd3eb359972d54a8b4130a564cd8baa87
Uz <2945921033@qq.com>
2024-06-14 16:32:32 +0800

    Fix implementation Error

--- src/main/kotlin/kt/uz/javasiliaosystem/JavaSiliaoSystemApplication.kt
+++ src/main/kotlin/kt/uz/javasiliaosystem/JavaSiliaoSystemApplication.kt
@@ -1,7 +1,9 @@
 package kt.uz.javasiliaosystem
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@SpringBootApplication
 class JavaSiliaoSystemApplication
 
 fun main(args: Array<String>) {
