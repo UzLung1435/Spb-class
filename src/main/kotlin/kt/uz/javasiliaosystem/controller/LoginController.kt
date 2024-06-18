8aa75c4d3c51f91e1442cb63691cabae51e7dbd9
Uz <2945921033@qq.com>
2024-06-18 20:50:17 +0800

    Shapshot

--- src/main/kotlin/kt/uz/javasiliaosystem/controller/LoginController.kt
+++ src/main/kotlin/kt/uz/javasiliaosystem/controller/LoginController.kt
@@ -1,6 +1,7 @@
 package kt.uz.javasiliaosystem.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController
 @Controller
 @RestController
 class LoginController {
-    @RequestMapping("/")
-    fun onResponseLogin(): String {
-        return "redirect:index.html"
+    @RequestMapping("/login")
+    fun onResponseLogin(model: Model): String {
+        model.addAttribute("Title","LoginTitle")
+        return "login"
     }
 }
